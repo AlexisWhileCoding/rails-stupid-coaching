fe5903768739b86c3022c01b9eeae71a1bf2e6af
Alexis Duchatelet <alexis.duchatelet@gmail.com>
2020-08-10 14:17:54 +0200

    functional

---
+++ app/controllers/answers_controller.rb
@@ -0,0 +1,14 @@
+class AnswersController < ApplicationController
+  def answer
+    if params[:question] == 'I am going to work'
+      @question = params[:question]
+      @answer = 'Great !'
+    elsif params[:question].include? "?"
+      @question = params[:question]
+      @answer = 'Silly question, get dressed and go to work!'
+    else
+      @question = params[:question]
+      @answer = "I don't care, get dressed and go to work!"
+    end
+  end
+end
